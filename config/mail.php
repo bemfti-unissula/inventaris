38d390494f8c59a343e2685166e46d5150a1fde6
mhusainh <husainhasbulah@gmail.com>
2025-08-07 15:49:14 +0700

    feat(mail): add Mailjet integration for email services
    Add Mailjet package and configure it as a mailer provider. Includes:
    - Mailjet service configuration in services.php
    - Mailjet mailer configuration in mail.php
    - Mailjet service provider registration
    - Composer dependency updates

--- config/mail.php
+++ config/mail.php
@@ -37,6 +37,30 @@ return [
 
     'mailers' => [
 
+        'mailjet' => [
+            'transport' => 'mailjet',
+            'key' => env('MAILJET_APIKEY'),
+            'secret' => env('MAILJET_APISECRET'),
+            'transactional' => [
+                'call' => true,
+                'options' => [
+                    'url' => 'api.mailjet.com',
+                    'version' => 'v3.1',
+                    'call' => true,
+                    'secured' => true
+                ]
+            ],
+            'common' => [
+                'call' => true,
+                'options' => [
+                    'url' => 'api.mailjet.com',
+                    'version' => 'v3',
+                    'call' => true,
+                    'secured' => true
+                ]
+            ]
+        ],
+
         'smtp' => [
             'transport' => 'smtp',
             'scheme' => env('MAIL_SCHEME'),
